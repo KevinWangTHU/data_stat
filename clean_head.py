a877a07810c3c58862e5125ea166c95715789ba8
Mingdong Wang <kevinwangthu@gmail.com>
2016-03-03 12:15:18 +0800

    clean cnn head

--- clean_head.py
+++ clean_head.py
@@ -42,10 +42,10 @@ def op(paras):
     return paras[beginPoint+delta:]
 
 
-if __name__ == "__main__":
+def clean_dm():
     count = 0
     in_dir = "../stories/cleaned_dm/"
-    out_dir = "../stories/cleaneded_dm/"
+    out_dir = "../stories/final_dm/"
     for file_name in os.listdir(in_dir):
         if file_name.endswith(".story"):
             count += 1
@@ -62,3 +62,24 @@ if __name__ == "__main__":
             fo = open(out_dir+file_name, "w")
             for i in range(len(output)):
                 fo.write(output[i])
+
+def clean_cnn():
+    count = 0
+    in_dir = "../stories/cleaned_cnn/"
+    out_dir = "../stories/final_cnn/"
+    for file_name in os.listdir(in_dir):
+        if file_name.endswith(".story"):
+            count += 1
+            if count % 500 == 0:
+                print count
+            f = open(in_dir+file_name).readlines()
+            fo = open(out_dir+file_name, "w")
+            if "(CNN) -- " in f[0]:
+                fo.write(f[0].split("(CNN) -- ")[1])
+            else:
+                fo.write(f[0])
+            for i in range(1, len(f)):
+                fo.write(f[i])
+
+if __name__ == "__main__":
+    clean_cnn()
--- compare.py
+++ compare.py
@@ -1,8 +1,8 @@
 import os
 
 if __name__ == "__main__":
-    in_dir = "../stories/cleaned_dm/"
-    out_dir = "../stories/cleaneded_dm/"
+    in_dir = "../stories/cleaned_cnn/"
+    out_dir = "../stories/final_cnn/"
     for fileName in os.listdir(in_dir):
         print fileName
         if fileName.endswith(".story"):
